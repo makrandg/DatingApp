2c9a848c46ee1b316ad97e7543af094872b4d4ce
makrandg <makrandg@gmail.com>
2018-12-28 14:43:13 +0530

    End Of section 3

--- Dating.API/Controllers/AuthController.cs
+++ Dating.API/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +18,9 @@ namespace Dating.API.Controllers
     {
         private readonly IAuthRepository _repo;
 
-        public IConfiguration _config { get; }
+        private IConfiguration _config { get; }
 
-        public AuthController(IAuthRepository repo,IConfiguration config)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
             _config = config;
@@ -47,18 +49,32 @@ namespace Dating.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.UserName,userForLoginDto.Password);
+            var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
 
-            if(userFromRepo == null)
-             return Unauthorized();
+            if (userFromRepo == null)
+                return Unauthorized();
 
-             var claims = new []
-             {
+            var claims = new[]
+            {
                  new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
                  new Claim(ClaimTypes.Name,userFromRepo.UserName.ToString())
              };
-             
-             var key =new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDesscritor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDesscritor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token) });
 
         }
 
--- Dating.API/Dtos/UserForRegisterDto.cs
+++ Dating.API/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace Dating.API.Dtos
         public string UserName { get; set; }        
         
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage="You must specify paasword between 4 and 8")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage="You must specify paasword between 4 and 8")]
         public string Password { get; set; }
     }
 }
